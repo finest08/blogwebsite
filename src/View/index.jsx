2de5db4bc88f22d726bf5f3a44a58d0f4e359ce0
finest08 <mr.mitcholl@gmail.com>
2022-08-14 15:59:14 +1200

    removed unnecessary code

--- src/View/index.jsx
+++ src/View/index.jsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
+import React from 'react';
 
 import Toolbar from '@mui/material/Toolbar';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
@@ -10,11 +9,12 @@ import { useParams } from "react-router-dom";
 
 import { query } from '../client';
 import { Hero } from '../components/Hero'
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 import { Outline } from '../components/Outline'
 import { Centerd, Left } from '../components/Section';
 import { Scroll } from '../components/Scroll';
+import { Grid } from '@mui/material';
 
 const slug = () => {
     const { view } = useParams();
@@ -24,40 +24,19 @@ const slug = () => {
     return view
 }
 
-const getWidth = () => window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
-
 export const View = (props) => {
     const view = slug()
     const { data, loading } = useQuery(query);
-    let [width, setWidth] = useState(getWidth());
 
     const viewFilter = data && data.assembly.reference.filter(item => item.name === view)
     const header = viewFilter && viewFilter[0].reference.filter(i => i.__typename === "Hero")
     const section = viewFilter && viewFilter[0].reference.filter(i => i.__typename === "Section")
 
-    useEffect(() => {
-        const resizeListener = () => {
-            // change width from the state object
-            setWidth(getWidth())
-        };
-        // set resize listener
-        window.addEventListener('resize', resizeListener);
-
-        // clean up function
-        return () => {
-            // remove resize listener
-            window.removeEventListener('resize', resizeListener);
-        };
-    }, [])
-
     return (
-        <>
-        <Toolbar id="back-to-top-anchor" />
-        <Container maxWidth='lg' sx={{ mt: 5}}>
-            {width > 400 && (
-                <>
+        <Grid container direction="column" justifyContent="center" alignItems="center">
+            <Toolbar id="back-to-top-anchor" />
+            <Grid item >
+                <Box>
                     {header &&
                         <Hero content={{
                             heroPrimary: header[0].heroPrimary,
@@ -76,35 +55,14 @@ export const View = (props) => {
                         }}
                         />
                     }
-                </>
-                )}
+                </Box>
                 <Scroll {...props}>
                     <Fab size="small" color="secondary" aria-label="scroll back to top">
                         <KeyboardArrowUpIcon />
                     </Fab>
-                 </Scroll>
-            {width < 300 && (
-                <>
-                    {header &&
-                        <Hero content={{
-                            heroPrimary: header[0].heroPrimary,
-                            heroSecondary: header[0].heroSecondary,
-                            altHeader: header[0].altHeader,
-                            heroSubHeader: header[0].heroSubHeader,
-                        }}
-                        />
-                    }
-                    {section &&
-                        <Left content={{
-                            header: section[0].header,
-                            description: section[0].description,
-                            section: section[0].section
-                        }} />
-                    }
-                </>
-                )}
-            <Outline visible={loading} />
-            </Container>
-        </>
+                </Scroll>
+                <Outline visible={loading} />
+            </Grid>
+        </Grid>
     )
 }
